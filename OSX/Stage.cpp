09be1beeb5f7dfc700a74bdb89ca3d12954c4998
cameCYR <yinghanw@andrew.cmu.edu>
2023-11-21 18:34:38 -0500

    init

--- OSX/Stage.cpp
+++ OSX/Stage.cpp
@@ -1,1 +1,23 @@
 #include "Stage.h"
+
+void InitMap(void){
+    // load map here
+    // open txt file and load the characters into map array
+    for (int i=0; i<WINDOW_WID; i++){
+        for (int j=0; j<WINDOW_HEI; j++){
+            // read blablabla
+            // map[i][j] = something
+        }
+    }
+}
+
+bool IsBlockPlacable(int player_x, int player_y, int direction){
+    if(map[player_x][player_y] == air)
+}
+bool BuildBlock(int blocktype);
+bool DestroyTerrain(int x, int y);
+bool IsCollisionWithTerrain(int projectile_x, int projectile_y);
+
+bool IsMoveOK(int player_x, int player_y, char key);
+void Draw(void);
+void PlayStageSoundEffect(void);
--- OSX/Stage.h
+++ OSX/Stage.h
@@ -3,8 +3,26 @@
 #ifndef Stage_H
 #define Stage_H
 
+#define WINDOW_WID (800)
+#define WINDOW_HEI (600)
+
+enum terrainType {grace, mud, air, wood, brick, water};
+unsigned char map[WINDOW_WID][WINDOW_HEI];
+
+class Terrain
+{
+    void InitMap(void);
+    bool IsBlockPlacable(int player_x, int player_y, int direction);
+    bool BuildBlock(int blocktype);
+    bool DestroyTerrain(int x, int y);
+    bool IsCollisionWithTerrain(int projectile_x, int projectile_y);
+};
+
 class Stage
 {
+    bool IsMoveOK(int player_x, int player_y, char key);
+    void Draw(void);
+    void PlayStageSoundEffect(void);
 };
 
 #endif
