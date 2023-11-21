b0525c4186362a150d94a284e9dfd4b192116adb
cameCYR <yinghanw@andrew.cmu.edu>
2023-11-21 18:42:16 -0500

    function design

--- OSX/Stage.cpp
+++ OSX/Stage.cpp
@@ -12,12 +12,26 @@ void InitMap(void){
 }
 
 bool IsBlockPlacable(int player_x, int player_y, int direction){
+    // check the block type in front of the main character to see if we can put a new block in front of it
+    // IMPORTANT:
+    // also check if there is a monster occupying the spot
+    // something like: if the block is air && if the block is not occupied then the spot is available to put a new block
     if(map[player_x][player_y] == air)
 }
-bool BuildBlock(int blocktype);
-bool DestroyTerrain(int x, int y);
-bool IsCollisionWithTerrain(int projectile_x, int projectile_y);
+bool BuildBlock(int blocktype){
+    // put a new block in front of the main character
+}
+bool DestroyTerrain(int x, int y){
+    // some blocks are designed to be destroyable by monster or projectile
+    // Destroy the block here
+}
+bool IsCollisionWithTerrain(int projectile_x, int projectile_y){
+    // check if the projectile hits the terrain
+    // this is a interface designed for projectile class to call
+}
 
-bool IsMoveOK(int player_x, int player_y, char key);
+bool IsMoveOK(int player_x, int player_y, char key){
+    
+}
 void Draw(void);
 void PlayStageSoundEffect(void);
